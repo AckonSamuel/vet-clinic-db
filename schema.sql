4bd54a6119e953826a461e6016997d4e407bd02c
Samuel Ackon <ackons433@gmail.com>
2022-09-16 16:13:52 +0000

    Alter animals table and add foreign keys

--- schema.sql
+++ schema.sql
@@ -24,3 +24,7 @@ CREATE TABLE species (
     name VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD  owner_id  INT REFERENCES owners(id);
