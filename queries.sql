e5718cfc8ce7fdaf1dfae5ce2b5c874217feada8
Samuel Ackon <ackons433@gmail.com>
2022-09-16 03:00:49 +0000

    Add fourth transaction

--- queries.sql
+++ queries.sql
@@ -36,3 +36,20 @@ ROLLBACK;
 
 SELECT * FROM animals;
 
+-- fourth transaction
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT del2022;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT del2022;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT * FROM animals;
